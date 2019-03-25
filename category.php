7a6346f95e3f3aba54cb0e09e5854a893f72de56
Anton Serednii <seredniy@gmail.com>
2019-03-25 13:37:39 +0200

    Небольшие фиксы шаблона категорий

--- category.php
+++ category.php
@@ -1,10 +1,25 @@
 <?php get_header(); ?>
-	<section>
-		<h1><?php single_cat_title(); ?></h1>
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/loop' ); ?>
-		<?php endwhile;
-		else: echo '<h2>Нет записей.</h2>'; endif; ?>
-	</section>
-<?php get_sidebar(); ?>
+
+<h1><?php single_cat_title(); ?></h1>
+
+<?php if ( category_description() ): ?>
+	<?php echo category_description(); ?>
+<?php endif; ?>
+
+<?php if ( have_posts() ) {
+	while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'template-parts/loop' ); ?>
+
+	<?php endwhile;
+} else {
+	echo "<div class='no-posts'>Пока тут ничего нет. Извините</div>";
+} ?>
+
+
+<?php if ( function_exists( 'pagination' ) ) {
+	pagination();
+} ?>
+
 <?php get_footer(); ?>
+
