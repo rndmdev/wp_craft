949d3f852ef61f7abf1873d86c33a358d23388bd
Anton Serednii <seredniy@gmail.com>
2019-05-08 13:31:28 +0300

    404 страница

--- 404.php
+++ 404.php
@@ -1,8 +1,13 @@
-<?php
+<?php get_header(); ?>
 
-get_header(); ?>
-
-	<h1>404 ошибка</h1>
-	<p>Страница не найдена!</p>
+	<div class="page-404">
+		<div class="page-404-descr">Ошибка</div>
+		<div class="page-404-title">404</div>
+		<div class="page-404-descr">Страница не найдена</div>
+		<div class="page-404-btn">
+			<button class="btn" onclick="window.history.back()">Вернуться назад</button>
+		</div>
+		<div class="page-404-link"><a href="/">На главную</a></div>
+	</div>
 
 <?php get_footer(); ?>
