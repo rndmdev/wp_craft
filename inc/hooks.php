fac78adcd2094570b9f46b2d9de9f11ae2ff729a
Anton Serednii <seredniy@gmail.com>
2019-10-25 15:47:56 +0300

    codemirror and wpautop #10

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,5 +1,26 @@
 <?php
 
 /* Отключаем емоджи */
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+/* Отключаем автоматическую вставку <p> и <br>  для CF7 */
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+/* Подключаем CodeMirror для редактора форм CF7*/
+add_action('admin_print_styles-toplevel_page_wpcf7', function () {
+
+    if (empty($_GET['post'])) {
+        return;
+    }
+    // Подключаем редактор кода для HTML.
+    $settings = wp_enqueue_code_editor(array('type' => 'text/html'));
+    // Ничего не делаем, если CodeMirror отключен.
+    if (false === $settings) {
+        return;
+    }
+    // Инициализация редактора для редактирования шаблона формы
+    wp_add_inline_script('code-editor', sprintf('jQuery( function() { wp.codeEditor.initialize( "wpcf7-form", %s ); } );', wp_json_encode($settings)));
+    // Инициализация редактора для редактирования шаблона письма
+    wp_add_inline_script('code-editor', sprintf('jQuery( function() { wp.codeEditor.initialize( "wpcf7-mail-body", %s ); } );', wp_json_encode($settings)));
+});
