e96de2e6ca96b7dc81936d599bd25214fa263372
alex shershen <rndmwork@gmail.com>
2019-11-08 16:00:39 +0200

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,25 @@
 /*Global variables*/
+function returnToTop() {
+    let btn = jQuery('.btn-top');
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() >= 50) {
+            btn.fadeIn(200);
+        } else {
+            btn.fadeOut(200);
+        }
+    });
+    btn.click(function () {
+        jQuery('body,html').animate({
+            scrollTop: 0
+        }, 500);
+    });
+}
 
 function initEvents() {
 
     /*Actions on 'DOM ready' event*/
     jQuery(function () {
-
+        returnToTop();
     });
 
     jQuery(window).resize(function () {
